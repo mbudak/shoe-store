5d8229abb28c27e44c45bef988d9533dbe25e397
mbudak <promaxcoq@gmail.com>
2022-06-02 21:06:12 -0400

    popup init

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
+/////////popup//////////////
+
+//declare default vars
+const popUp = document.getElementById('pop-up');
+const popUpLayer = document.getElementById('pop-up-layer');
+const closeModal = document.getElementById('close-pop-up');
+
+//nav-bar button
+const subBtn = document.getElementById('sub-btn');
 
 /*************
 testimonial section
@@ -119,3 +128,25 @@ if (panel.style.maxHeight){
 }
   });
 }
+
+//navbar button click makes the popup appear
+subBtn.addEventListener("click", () => {
+    popUpLayer.style.display = 'flex';
+  })
+  
+  //close model click makes the popup disappear
+  closeModal.addEventListener("click", function() {
+    popUpLayer.style.display = 'none';
+  })
+
+  let popUpSeconds = 40;
+
+//trigger popup after seconds variable
+let popUpFun = window.setInterval(function() {
+  popUpLayer.style.display = 'flex';
+
+  window.clearInterval(popUpFun);
+
+}, popUpSeconds * 1000);
+
+///////////////////
